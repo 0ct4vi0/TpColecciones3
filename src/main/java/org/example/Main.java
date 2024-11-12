2ff15949070475454e816e9b920c059f052627f1
Octavio Rolon <octaviorolonh@gmail.com>
2024-11-12 12:22:32 -0300

    a la mitad del trebajo 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,41 +4,95 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        menu();
-
+        HashMap<String, Long> agenda = new HashMap<>();
+        menu(agenda);
     }
 
-    public static  void  menu() {
+//Metodo menu de opciones------------------------------
+    public static  void  menu(HashMap<String, Long> agenda) {
         Scanner sc = new Scanner(System.in);
-        HashMap<String, Long> Agenda = new HashMap<String, Long>();
+
         System.out.println("---------MENU-----------");
-        System.out.println("Que desea realiza:" + "\n"
-                + "1)Cargar Datos en la Agenda." + "\n"
-                + "2)Buscar Telefono por Nombre." + "\n"
-                + "3)Lista Agenda.");
+        System.out.println("""
+                        Que desea realiza:
+                        1)Cargar Datos en la Agenda.
+                        2)Buscar Telefono por Nombre.
+                        3)Lista Agenda.
+                        4) Salir.""");
 
         int opcion = sc.nextInt();
         sc.nextLine();
-//        if (opcion != 1 || opcion != 2 || opcion != 3) {
-//            System.out.println("tiene que ingresar una de las opciones");
-         switch (opcion){
-             case 1:
-                System.out.println("-------------------------------");
-                System.out.println("ingrese  su nombre completo: ");
-                String nombre = sc.nextLine();
-                sc.nextLine();
-                System.out.println("ingrese su numero telefonico");
-                long numTel = sc.nextLong();
-                Agenda.put(nombre, numTel);
 
+         switch (opcion){
+             case 1://cargamos los datos
+                 System.out.println("----------------------");
+                 System.out.println("ingrese la cantidad de personas ingresar sus datos: ");
+                 int cantidad = sc.nextInt();
+                 sc.nextLine();
+                 for (int i = 0; i < cantidad; i++) {
+
+                     System.out.println("-------------------------------");
+                     System.out.println("ingrese  su nombre completo: ");
+                     String nombre = sc.nextLine();
+
+                     System.out.println("ingrese su numero telefonico");
+                     long numTel = sc.nextLong();
+                     sc.nextLine();
+
+                     // envio a validar los datos
+                     validarDtos(agenda, nombre, numTel);
+                 }
+                 System.out.println("---------------------");
+                 System.out.println("Volviendo al menu principal");
+                 menu(agenda);
              case 2:
-                 break;
-             case 3:
+                 System.out.println("ingrese el nombre de la persona que desea buscar");
+                 String nombre= sc.nextLine();
+
+
+
+                 buscarTelefono(agenda);
+             case 3: //mostramos la agenda
+                 mostrarAgenda(agenda);
+                 menu(agenda);
+             case 4:
+                 System.out.println("--------------------------------");
                  break;
              default:
                  System.out.println("datos ingresados invalidos, ingrese una de esas opciones");
-                 menu();
-                 System.out.println();
+                 menu(agenda);
+        }
+    }
+
+//metodo de validacion de datos------------------------------
+    public static void  validarDtos(HashMap<String, Long> agenda, String nombre, long numTel){
+        if (agenda.containsKey(nombre)){
+            System.out.println("El nombre "+ nombre+" ya esta registrado");
+        } else if (agenda.containsValue(numTel)) {
+            System.out.println("El telefono "+ numTel+" ya esta registrado al nombre de : "+nombre );
+        }else{
+            System.out.println("se registro correctamente");
+            agenda.put(nombre, numTel);
+        }
+    }
+
+    public static void buscarTelefono(HashMap<String, Long> agenda){
+
+    }
+
+//-------Metodo para mostrar la agenda-------------------------
+    public static void mostrarAgenda(HashMap<String, Long> agenda) {
+
+//        for (String i : agenda.keySet()) {
+//            System.out.println(i);
+//        }
+//        for (long i : agenda.values()) {
+//            System.out.println(i);
+//        }
+
+        //Mostramos la agenda
+        for (String i : agenda.keySet()) {
+            System.out.println("Nombre: " + i + " Telefono: " + agenda.get(i));
         }
     }
 }
